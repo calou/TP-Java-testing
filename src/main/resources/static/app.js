5241b515de40fd47fe1bc1bb8ae2495da8a96332
Sébastien Gruchet <sebastien.gruchet@digimind.com>
2019-03-23 20:22:21 +0100

    Add UI to create a transaction

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -11,18 +11,40 @@ bankApp.factory('Account', ['$resource',
       });
     }
 ]);
-bankApp.controller('AccountController', function AccountController($scope, Account) {
+bankApp.factory('Transaction', ['$resource',
+    function($resource) {
+      return $resource('/transaction', {}, {
+        save: {method: 'PUT'},
+        query: {
+          method: 'GET',
+          isArray: true
+        }
+      });
+    }
+]);
+bankApp.controller('AccountController', function AccountController($scope, Account, Transaction) {
   $scope.accounts = Account.query();
-  $scope.createAccountRequest = {
-    "account_id": "",
-    "balance": 0
-  }
-  $scope.create = function(account_id, balance) {
+  $scope.createAccountRequest = {};
+  $scope.createTransactionRequest = {};
+  $scope.createAccount = function(account_id, balance) {
     var account = new Account();
     account.account_id = account_id;
     account.balance = balance;
-    savePromise = account.$save(function(account, putResponseHeaders) {
+    account.$save(function(account, putResponseHeaders) {
         $scope.accounts = Account.query();
+        $scope.createAccountRequest = {};
     });
   }
+
+  $scope.createTransaction = function(debitor, creditor, category, amount) {
+      var transaction = new Transaction();
+      transaction.debitor = debitor;
+      transaction.creditor = creditor;
+      transaction.category = category;
+      transaction.amount = amount;
+      transaction.$save(function(transaction, putResponseHeaders) {
+          $scope.accounts = Account.query();
+          $scope.createTransactionRequest = {};
+      });
+    }
 });
